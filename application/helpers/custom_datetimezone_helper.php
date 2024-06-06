20d88e223a8b16549bf6ea13eba4a3cb0ec1667a
Munawar Shah <mwr.ali.shah@gmail.com>
2024-06-06 19:06:24 +0500

    Summary of changes updated in README.md / Availaibility Hours made to tend to nearest logical 30 min mark / POST endpoint called from frontend_book_api.js modified to return all providers

---
+++ application/helpers/custom_datetimezone_helper.php
@@ -0,0 +1,24 @@
+<?php
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class CustomDateTimeZone extends DateTimeZone {
+    private static $customTimezones = [
+        'S1' => '+07:00', 
+        'S2' => '+01:00', 
+        'S3' => '-05:00', 
+        'S4' => '-11:00', 
+    ];
+
+    public function __construct($timezone) {
+        if (isset(self::$customTimezones[$timezone])) {
+            $timezone = self::$customTimezones[$timezone];
+        }
+        parent::__construct($timezone);
+    }
+}
+
+// Define a global function to create CustomDateTimeZone instances
+function create_custom_datetimezone($timezone) {
+    return new CustomDateTimeZone($timezone);
+}
+
